f0b8ec2cfedcc0bcd4b5600adb77a669eac844f2
bean <jump09205@gmail.com>
2025-03-23 21:59:27 +0800

    feat 翻譯

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -13,10 +13,13 @@ const resources = {
     translation: zh_TW,
   },
 };
+
+const language = localStorage.getItem('user')
+
 i18n.use(initReactI18next).init({
   resources, // 所有翻譯資源
   fallbackLng: 'en_US', // 如果當前切換的語言沒有對應的翻譯則使用這個語言
-  lng: 'zh_TW', // 預設語言
+  lng: language ? JSON.parse(language).state.language : 'zh_TW', // 預設語言
   interpolation: {
 	  // 是否要讓字詞 escaped 來防止 xss 攻擊，這裡因為 React.js 已經做了，就設成 false即可
     escapeValue: false,
