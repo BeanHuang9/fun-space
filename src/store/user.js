4d35393a6267f2dfd8f5db3ff1741fa23c27f7f8
bean <jump09205@gmail.com>
2025-03-23 21:50:38 +0800

    feat 翻譯

--- src/store/user.js
+++ src/store/user.js
@@ -13,6 +13,8 @@ import { persist } from 'zustand/middleware';
 export const useUserStore = create(persist((set) => ({
     token: '',
     setToken: (token) => set(()=>({token})),
+    language: 'zh_TW',
+    setLanguage: (language) => set(() => ({ language }))
 }),{
     name:'user'
 }))
